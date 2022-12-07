e8622325daf74785a80e98af454470afe8380e3c
ZorilOlah <Zorilolah@gmail.com>
2022-12-07 12:04:58 +0100

    cuda

--- stablediff2.py
+++ stablediff2.py
@@ -6,7 +6,7 @@ model_id = "stabilityai/stable-diffusion-2"
 # Use the Euler scheduler here instead
 scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
 pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, revision="fp16", torch_dtype=torch.float16)
-#pipe = pipe.to("cuda")
+pipe = pipe.to("cuda")
 
 prompt = "a photo of an astronaut riding a horse on mars"
 image = pipe(prompt, height=768, width=768).images[0]
